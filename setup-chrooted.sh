8ba1b5a76933b2cb099ef2dcce133032b7619d57
Jaap Jansma <jaapjansma@users.noreply.github.com>
2016-08-19 11:01:14 +0200

    Create setup-chrooted.sh

---
+++ setup-chrooted.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+# This script has to be executed after a chroot
+
+cd /root
+git clone https://github.com/jaapjansma/arch-linux-server.git
+
+ln -s /usr/share/zoneinfo/Europe/Amsterdam /etc/localtime
+hwclock --systohc --utc
+
+sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g" /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
