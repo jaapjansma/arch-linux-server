39910ecca470b1a8ff69e9a220d3cf6374a768f0
Jaap Jansma <jaap@edeveloper.nl>
2016-09-09 15:25:06 +0200

    update

--- scripts/post-installation.sh
+++ scripts/post-installation.sh
@@ -3,7 +3,7 @@
 new_hostname=`cat /root/config/hostname`
 hostnamectl set-hostname ${new_hostname}
 
-/usr/local/bin/arch-linux-server/scripts/send-email-from-dir.py --directory=/root/mails
+python3 /usr/local/bin/arch-linux-server/scripts/send-email-from-dir.py --directory=/root/mails
 
 systemctl disable post-installation.service
 rm -rf /etc/systemd/system/post-installation.service
