fbf74c929994036f623a10c18af091983ebe578f
Jaap Jansma <jaapjansma@users.noreply.github.com>
2017-02-19 16:26:50 +0100

    Update only_outgoing.sh

--- mailserver/only_outgoing.sh
+++ mailserver/only_outgoing.sh
@@ -8,7 +8,9 @@ sudo echo "
 # Simple spam prevention. Taken from http://www.netarky.com/programming/arch_linux/Arch_Linux_mail_server_setup_1.html
 smtpd_helo_required = yes
 smtpd_helo_restrictions = reject_non_fqdn_helo_hostname, reject_unknown_helo_hostname
-smtpd_delay_reject = yes" >> /etc/postfix/main.cf
+smtpd_delay_reject = yes
+inet_interfaces = loopback-only
+" >> /etc/postfix/main.cf
 
 sudo echo "
 # Person who should get root's mail. Don't receive mail as root!
